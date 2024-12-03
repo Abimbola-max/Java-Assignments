07c0480b1a6a38a0dc8f6a338af402e0b77bf7e3
Abimbola <Haryishar2@gmail.com>
2024-12-03 11:03:12 +0100

    task1

--- classKata/phraseGate/StudentsGradeApp.java
+++ classKata/phraseGate/StudentsGradeApp.java
@@ -12,9 +12,8 @@ public class StudentsGradeApp {
 
     public static void forAllInput() {
        
-        System.out.println("Enter the number of pupils in your class: ");
+        System.out.println("Enter the number of students in your class: ");
         numberOfStudents = input.nextInt();
-
 
         double[][] studentScores = new double[numberOfStudents][]; 
 
@@ -25,7 +24,18 @@ public class StudentsGradeApp {
 
             noOfAllSubjects = input.nextInt();
 
-            
+	if (studentScores < 0 || studentScores > 100) {
+		System.out.println("Invalid");
+	
+	}
+            studentScores[count] = new double[noOfAllSubjects]; 
+		
+
+            System.out.println("Enter scores for student " + (count + 1) + ": ");
+
+            for (int counter = 0; counter < noOfAllSubjects; counter++) {
+                studentScores[count][counter] = input.nextDouble();
+            }
         }
 
       
