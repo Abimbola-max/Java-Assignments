26a7924292c84227b9552809cf555fb611f75123
Abimbola <Haryishar2@gmail.com>
2024-12-03 17:01:26 +0100

    tasks

--- classKata/phraseGate/StudentsGradeApp.java
+++ classKata/phraseGate/StudentsGradeApp.java
@@ -18,6 +18,8 @@ public class StudentsGradeApp {
 
         double[][] studentScores = new double[numberOfStudents][];
 
+	
+
         for (int count = 0; count < numberOfStudents; count++) {
 
             System.out.println("Enter number of subjects for student" + (count + 1) + ": ");
@@ -28,26 +30,27 @@ public class StudentsGradeApp {
             System.out.println("Enter scores for student " + (count + 1) + ": ");
 
             
-            double total = 0; 
+            double total = 0;
+	    double firstMax = studentScores[0][0];
 
             for (int counter = 0; counter < noOfAllSubjects; counter++) {
                 studentScores[count][counter] = input.nextDouble();
                 total += studentScores[count][counter];
             }
-		for
+		for(int counter = 0; counter < noOfAllSubjects; counter++) {
+			for(int index = counter; index < noOfAllSubjects; index++) {
+		if (studentScores[0][0] > firstMax) {
+                    firstMax = studentScores[count][counter];
+                    
+                }
+		}
+	}
+	
+	    
 
             System.out.println("The total grade for student " + (count + 1) + " is: " + total);
 
             System.out.println("The average grade for student " + (count + 1) + " is: " + (double) (total / noOfAllSubjects));
-	
-	System.out.print("=====================================\n");
-	System.out.print("STUDENT\tSUB1\tSUB2\tSUB3\tTOT\tAVERAGE\n");
-	System.out.print("=====================================\n");
-	System.out.print("student1\t\tSUB2\tSUB3\tTOT\tAVERAGE\n");
-	System.out.print("student1\t\tSUB2\tSUB3\tTOT\tAVERAGE\n");
-	System.out.print("student1\t\tSUB2\tSUB3\tTOT\tAVERAGE\n");
-
-
 		
 	}
      }
