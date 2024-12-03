79c4da18d0ae746763390b1b0f5576c91d4379e2
Abimbola <Haryishar2@gmail.com>
2024-12-03 13:28:30 +0100

    task3

--- classKata/phraseGate/StudentsGradeApp.java
+++ classKata/phraseGate/StudentsGradeApp.java
@@ -5,11 +5,10 @@ public class StudentsGradeApp {
     static Scanner input = new Scanner(System.in);
     static int numberOfStudents;
     static int noOfAllSubjects;
-    static double total = 0;
 
     public static void main(String[] args) {
         forAllInput();
-	sampleTable();
+        
     }
 
     public static void forAllInput() {
@@ -20,7 +19,6 @@ public class StudentsGradeApp {
         double[][] studentScores = new double[numberOfStudents][];
 
         for (int count = 0; count < numberOfStudents; count++) {
-            
 
             System.out.println("Enter number of subjects for student" + (count + 1) + ": ");
             noOfAllSubjects = input.nextInt();
@@ -29,25 +27,28 @@ public class StudentsGradeApp {
 
             System.out.println("Enter scores for student " + (count + 1) + ": ");
 
+            
+            double total = 0; 
+
             for (int counter = 0; counter < noOfAllSubjects; counter++) {
                 studentScores[count][counter] = input.nextDouble();
                 total += studentScores[count][counter];
             }
+		for
 
-		System.out.println("The total grade for student " + (count + 1) + " is: " + total);
+            System.out.println("The total grade for student " + (count + 1) + " is: " + total);
 
-            System.out.println("The average grade for student " + (count + 1) + " is: " + (double)(total/noOfAllSubjects));
-        }
-    }
-
-	public static void sampleTable() {
+            System.out.println("The average grade for student " + (count + 1) + " is: " + (double) (total / noOfAllSubjects));
 	
 	System.out.print("=====================================\n");
-	System.out.print("STUDENT\tSUB1\tSUB2\tSUB3\tTOT\tAVE\tPOS\n");
+	System.out.print("STUDENT\tSUB1\tSUB2\tSUB3\tTOT\tAVERAGE\n");
 	System.out.print("=====================================\n");
+	System.out.print("student1\t\tSUB2\tSUB3\tTOT\tAVERAGE\n");
+	System.out.print("student1\t\tSUB2\tSUB3\tTOT\tAVERAGE\n");
+	System.out.print("student1\t\tSUB2\tSUB3\tTOT\tAVERAGE\n");
 
 
+		
 	}
-
-
+     }
 }
