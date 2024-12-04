c180050e4651270db501b0cf0522a6590a0889d9
Abimbola <Haryishar2@gmail.com>
2024-12-04 18:05:51 +0100

    tasks

--- classKata/phraseGate/CreditCardValidation.java
+++ classKata/phraseGate/CreditCardValidation.java
@@ -6,40 +6,73 @@ public class CreditCardValidation {
 
         Scanner input = new Scanner(System.in);
         
-       		System.out.println("Credit card numbers; ");
+       		System.out.println("Hello, Kindly enter Card details to verify: ");
 		String cardNumber = input.next();
 
+		System.out.println("**************************************");
+
 		if (cardNumber.startsWith("4")) {
-			System.out.println("You are using a visa card");
+			System.out.println("**Credit Card Type: Visa card");
 
 		} else if (cardNumber.startsWith("5")) {
-			System.out.println("You are using MasterCard");
+			System.out.println("**Credit Card Type: MasterCard");
 
 		} else if (cardNumber.startsWith("37")) {
-			System.out.println("You are using American Express cards"); 
+			System.out.println("**Credit Card Type: American Express cards"); 
 
 		} else if (cardNumber.startsWith("6")) {
-			System.out.println("You are using Discover card");
+			System.out.println("**Credit Card Type: Discover card");
 		} else {
 			System.out.println("Invalid details");
 		}
+
+        if (isValid(cardNumber)) {
+            System.out.println("**Credit card validity Status: " + "valid");
+        } else {
+            System.out.println("**Credit card validity Status: " + "invalid");
+        }
+	System.out.println("**Credit Card Number: " + cardNumber);
+        System.out.println("**Credit Card Digit Length: " + cardNumber.length());
+       System.out.println("**************************************");
+   
 	}
+     
+    public static boolean isValid(String cardNumber) {
+         
+        if (!(cardNumber.length() >= 13 && cardNumber.length() <= 16)) {
+            return false;
+        }
+
+        int evenSum = sumOfDoubleEvenPlace(cardNumber);
+        int oddSum = sumOfOddPlace(cardNumber);
+        return (evenSum + oddSum) % 10 == 0;
+    }
+
      
     public static int sumOfDoubleEvenPlace(String cardNumber) {
         int sum = 0;
-        for (int  = cardNumber.length() - 2; countForEven >= 0; countForEven -= 2) {
-            int digit = Integer.parseInt(cardNumber.substring(countForEven, countForEven + 2));
+        for (int countForEven = cardNumber.length() - 2; countForEven >= 0; countForEven -= 2) {
+            int digit = Integer.parseInt(cardNumber.substring(countForEven, countForEven + 1));
             int doubledDigit = digit * 2;
             sum += getDigit(doubledDigit);
         }
         return sum;
     }
 
+    
+    public static int getDigit(int number) {
+        if (number > 9) {
+            return (number - 9);
+        } else {
+            return number;
+        }
+    }
 
+ 
     public static int sumOfOddPlace(String cardNumber) {
         int sum = 0;
-        for (int countForEven = cardNumber.length() - 1; countForEven >= 0; countForEven -= 2) {
-            sum += Integer.parseInt(cardNumber.substring(countForEven, countForEven + 1));
+        for (int countForodd = cardNumber.length() - 1; countForodd >= 0; countForodd -= 2) {
+            sum += Integer.parseInt(cardNumber.substring(countForodd, countForodd + 1));
         }
         return sum;
     }
