b61cfd84f1eea6359d186fca7216d4346126b29d
Abimbola <Haryishar2@gmail.com>
2024-12-31 02:32:44 +0100

    tasks

--- classKata/checkoutapp/CheckOutAppTest.java
+++ classKata/checkoutapp/CheckOutAppTest.java
@@ -1,22 +1,19 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
 
+public class CheckOutApplicationTest {
 
+    @Test
+    public void calculateAndDisplayTotals() {
 
-public class CheckOutAppTest {
-	
-	@Test
-	public void testAddProduct() {
-        CheckOutApp.inputProductDetails("Apple", 1.0, 2);
-        CheckOutApp.inputProductDetails("Banana", 0.5, 3);
+        String name = "Tayo";
+        double discount = 0.8;
+        double newDiscountPrice = discount;
+        double total = 3000;
 
-        assertEquals(Arrays.asList("Apple", "Banana"), CheckOutApp.productNames);
-        assertEquals(Arrays.asList(1.0, 0.5), CheckOutApp.productPrices);
-        assertEquals(Arrays.asList(2, 3), CheckOutApp.productQuantities);
-
-	}
-	
+        double expected = 3400;
+        double actual = total - newDiscountPrice + newVatPrice;
+        assertEquals(expected, actual);
+    }
 }
