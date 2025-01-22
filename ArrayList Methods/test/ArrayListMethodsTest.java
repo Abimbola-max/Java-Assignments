c861114aeeb1c264160d2de3e92b7c8f17d04f9d
Abimbola <Haryishar2@gmail.com>
2025-01-22 02:15:23 +0100

    tasks

--- ArrayList Methods/test/ArrayListMethodsTest.java
+++ ArrayList Methods/test/ArrayListMethodsTest.java
@@ -6,28 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ArrayListMethodsTest {
-    ArrayListMethod arrayListMethod;
+    ArrayListMethod arrayListMethods;
 
     @BeforeEach
     public void startWithThis() {
-        arrayListMethod = new ArrayListMethod();
+        arrayListMethods = new ArrayListMethod();
     }
 
     @Test
     public void testThatArrayListIsEmpty() {
-        ArrayListMethod arrayListMethods = new ArrayListMethod();
         assertTrue(arrayListMethods.isEmpty());
     }
 
     @Test
     public void testThatArrayListIsNotEmpty() {
-        ArrayListMethod arrayListMethods = new ArrayListMethod();
         assertFalse(arrayListMethods.isNotEmpty());
     }
 
     @Test
     public void addElementToArrayList() {
-        ArrayListMethod arrayListMethods = new ArrayListMethod();
         arrayListMethods.add("bibi");
     }
 
