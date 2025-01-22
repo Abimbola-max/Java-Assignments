706eeb58fcd259c46d84e641ec6750f207cefa76
Abimbola <Haryishar2@gmail.com>
2025-01-22 04:20:35 +0100

    tasks

---
+++ Set Methods/test/SetMethodsTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMethodsTest {
+
+    private SetMethods setMethods;
+
+    @BeforeEach
+    public void startsWithThis() {
+        setMethods = new setMethods();
+    }
+
+    @Test
+    public void testSetsIsEmpty() {
+
+    }
+}
