a19b9f207fe8e1f170f487cd8cbdcc886c74768b
Abimbola <Haryishar2@gmail.com>
2025-01-27 15:41:23 +0100

    task

--- Bank App/tests/TestAccount.java
+++ Bank App/tests/TestAccount.java
@@ -1,4 +1,10 @@
+import bankapp.Account;
+import org.junit.jupiter.api.Test;
+
 public class TestAccount {
 
     @Test
+    public void testThatAccountBalanceIsZero() {
+        Account account = new Account();
+    }
 }
