5860981f9cd2aa702921ef78e281627ef47e0f13
Abimbola <Haryishar2@gmail.com>
2025-01-27 15:49:32 +0100

    task

--- Bank App/tests/TestAccount.java
+++ Bank App/tests/TestAccount.java
@@ -1,10 +1,13 @@
 import bankapp.Account;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestAccount {
 
     @Test
     public void testThatAccountBalanceIsZero() {
         Account account = new Account();
+        assertTrue(account.isEmpty());
     }
 }
--- Bank App/tests/bankapp/Account.java
+++ Bank App/tests/bankapp/Account.java
@@ -1,4 +1,20 @@
 package bankapp;
 
 public class Account {
+
+    private int accountNumber;
+    private String firstName;
+    private String lastName;
+    private int pin;
+
+    public Account() {
+        this.accountNumber = accountNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pin = pin;
+    }
+
+    public boolean isEmpty() {
+        return accountNumber == 0;
+    }
 }
