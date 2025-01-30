d9916b75c2039443b8e36251b9eafbb74e74f3cc
Abimbola <Haryishar2@gmail.com>
2025-01-30 15:00:19 +0100

    classtasks

--- Diary App/src/diaryapp/Entry.java
+++ Diary App/src/diaryapp/Entry.java
@@ -1,5 +1,16 @@
 package diaryapp;
 
 public class Entry {
-    
+
+    private int id;
+    private String title;
+    private String body;
+    private String date;
+
+    public Entry(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
 }
