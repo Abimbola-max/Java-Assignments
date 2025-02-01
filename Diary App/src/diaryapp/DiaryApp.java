3303f40fc1ed1ba0ff65b8345f78695ee342b1c4
Abimbola <Haryishar2@gmail.com>
2025-02-01 02:51:32 +0100

    tasks

--- Diary App/src/diaryapp/DiaryApp.java
+++ Diary App/src/diaryapp/DiaryApp.java
@@ -1,12 +1,16 @@
 package diaryapp;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DiaryApp {
 
     static Scanner input = new Scanner(System.in);
+    private Diary diary;
 
     public static void main(String[] args) {
+        DiaryApp app = new DiaryApp();
+        app.mainOptions();
 
     }
 
@@ -33,10 +37,43 @@ public class DiaryApp {
     }
 
     public void createDiary() {
+        try {
+            System.out.println("Enter Your username: ");
+            String username = input.next();
+            System.out.println("Enter Your password: ");
+            String password = input.next();
+            System.out.println("Confirm your password: ");
+            String confirmPassword = input.next();
+
+            if (password.equals(confirmPassword)) diary = new Diary(username, password);
+        } catch (NullPointerException exception) {
+            System.out.println("Username or password can not be empty.");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Username or password is invalid.");
+        } finally {
+            mainOptions();
+        }
+    }
+
+    public void lockDiary() {
+        try {
+            System.out.println("Enter Your password: ");
+            String password = input.next();
+        } catch (InputMismatchException exception) {
+            System.out.println("Password can not be empty.");
+        }
+    }
+
+    public void createEntry() {
         System.out.println("Enter Diary Title: ");
         String title = input.nextLine();
         System.out.println("Enter the body or description of your diary: ");
         String body = input.nextLine();
-        
+        try {
+            diary = new Diary(title, body);
+
+
+        }
     }
+
 }
