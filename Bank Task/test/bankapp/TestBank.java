638c4cfc0223e68eaeb3d8f1c919e1050cd54617
Abimbola <Haryishar2@gmail.com>
2025-02-06 03:09:54 +0100

    task

--- Bank Task/test/bankapp/TestBank.java
+++ Bank Task/test/bankapp/TestBank.java
@@ -2,11 +2,25 @@ package bankapp;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestBank {
 
     @Test
-    public void TestBankCanCreateAccountForAuser() {
+    public void TestBankCanCreateAccountForAuserAndReturnOneAsNumberOfAccountCreated() {
         Bank bank = new Bank();
         bank.createAccount("firstName", "lastName", "password");
+
+        assertEquals(1, bank.getAccounts().size());
+    }
+
+    @Test
+    public void testThatBankCanCreateAccountForThreeusersAndReturnsThreeAsNumberOfAccountCreated() {
+        Bank bank = new Bank();
+        bank.createAccount("firstName", "lastName", "password");
+        bank.createAccount("firstName1", "lastName1", "password1");
+        bank.createAccount("firstName2", "lastName2", "password2");
+
+        assertEquals(3, bank.getAccounts().size());
     }
 }
