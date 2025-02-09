6c61e2149b2b938091f28e78005093e073bf545a
Abimbola <Haryishar2@gmail.com>
2025-02-09 12:43:25 +0100

    task

--- E-Store/src/estore/User.java
+++ E-Store/src/estore/User.java
@@ -1,4 +1,11 @@
 package estore;
 
 public class User {
+
+    private int age;
+    private String email;
+    private String homeAddress;
+    private String name;
+    private String password;
+    private String phone;
 }
